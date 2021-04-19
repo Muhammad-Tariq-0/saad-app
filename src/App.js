d046afa243318f62a19912cbc69c4c28293e8d3a
Muhammad Tariq <muhammad64tariq@gmail.com>
2021-04-19 22:36:09 +0500

    checking

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 const App = () => {
+  window.open("https://baerelationship.com/", "_self");
   return (
     <div>
-      <h1>Saad App</h1>
+      {/* <h1>Saad App</h1> */}
     </div>
   )
 }
